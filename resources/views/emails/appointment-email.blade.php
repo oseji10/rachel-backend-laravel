9b9479989b1bde2f3ce68267f494828012c44851
oseji10 <57804483+oseji10@users.noreply.github.com>
2025-02-01 05:13:28 +0100

    updates

--- resources/views/emails/appointment-email.blade.php
+++ resources/views/emails/appointment-email.blade.php
@@ -437,7 +437,7 @@
   <![endif]-->
   </head>
   <body>
-    <span class="preheader">Thanks for registering on iLearn Africa. We’ve pulled together some information and resources to help you get started.</span>
+    <!-- <span class="preheader">Thanks for registering on iLearn Africa. We’ve pulled together some information and resources to help you get started.</span> -->
     <table class="email-wrapper" width="100%" cellpadding="0" cellspacing="0" role="presentation">
       <tr>
         <td align="center">
