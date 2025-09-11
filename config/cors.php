f2ffa21460f3da877c79161b53c87aa7117159c7
Victor Oseji <vctroseji@gmail.com>
2025-09-11 04:57:28 +0100

    update

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,8 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    // 'allowed_origins' => ['*'],
+    'allowed_origins' => ['*', 'http://localhost:3003'],
 
     'allowed_origins_patterns' => [],
 
